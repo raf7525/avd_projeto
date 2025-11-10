19f8f9c2d08e30169dc67bebf990a0a649815bfa
Raf75 <rafaplanisa37@gmail.com>
2025-11-10 09:13:18 -0300

    fiz o básico do tratamento de dados do dataset

--- notebooks/tratamento.py
+++ notebooks/tratamento.py
@@ -1,2 +1,25 @@
 import pandas as pd
 import numpy as np
+
+
+def removendo_duplicadas(df):
+    return df.drop_duplicates()
+
+def padronizando_coluas(df):
+    df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('-', '_')
+    return df
+
+def tratando_linhas_nulas(df):
+    return df.dropna()
+##caso não se tenha muitos dados, adicionar um cálculo de fazer a média com base em caracteristicas
+
+
+
+##eh necessario checar se a remocao de outliers eh o ideal, talvez eles sejam importantes
+def remove_outliers(df, column,factor=1.5):
+        Q1 = df[column].quantile(0.25)##esses valores são o máximo e mínimo aceitáveis, talvez seja necessário mudar diante da necessidade
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        limite_inferior = Q1 - factor * IQR
+        limite_maximo = Q3 + factor * IQR
+        return df[(df[column] >= limite_inferior) & (df[column] <= limite_maximo)]
