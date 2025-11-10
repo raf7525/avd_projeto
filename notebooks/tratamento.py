b16af77a2f433e82941f0319350264df7272a54a
Rafa <ralb@cesar.school>
2025-11-10 17:51:37 -0300

    implementei o MinMaxScaller

--- notebooks/tratamento.py
+++ notebooks/tratamento.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import MinMaxScaler
 
 def removendo_duplicadas(df):
     return df.drop_duplicates()
@@ -13,8 +13,6 @@ def tratando_linhas_nulas(df):
     return df.dropna()
 ##caso não se tenha muitos dados, adicionar um cálculo de fazer a média com base em caracteristicas
 
-
-
 ##eh necessario checar se a remocao de outliers eh o ideal, talvez eles sejam importantes
 def remove_outliers(df, column,factor=1.5):
         Q1 = df[column].quantile(0.25)##esses valores são o máximo e mínimo aceitáveis, talvez seja necessário mudar diante da necessidade
@@ -23,3 +21,11 @@ def remove_outliers(df, column,factor=1.5):
         limite_inferior = Q1 - factor * IQR
         limite_maximo = Q3 + factor * IQR
         return df[(df[column] >= limite_inferior) & (df[column] <= limite_maximo)]
+
+def normalizando_dados(df,colunas_numericas =None):
+     if colunas_numericas is None:
+          colunas_numericas = ['velocity', 'direction', 'temperature', 'humidity']##alterar diante das colunas quando tivermos os dados
+          scaler = MinMaxScaler()
+          df_normalizado = df.copy()
+          df_normalizado[colunas_numericas] = scaler.fit_transform(df[colunas_numericas])
+          return df_normalizado, scaler
